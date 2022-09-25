d91877a0a17c1a22bdcfd454571dcfb8cf28aab2
MikhailALKir <mixa_kirillov_53@mail.ru>
2022-09-25 14:47:54 +0300

    Исправление недочетов

--- main.py
+++ main.py
@@ -10,15 +10,15 @@ for index, row in enumerate(table):
     if index == 0:
         continue
 
-    total_count += 1
     title = row[1]
 
     if len(title) > 30:
         short_title_count += 1
+
     total_count += 1
 
 print("Кол-во книг: ", total_count)
-print("Кол-во книг  меньше 30 символов: ", short_title_count)
+print("Кол-во книг больше 30 символов: ", short_title_count)
 
 search_author = input('Введите автора: ').lower()
 
@@ -35,7 +35,7 @@ for index, row in enumerate(table):
     author = row[3]
 
     if author.lower() == search_author:
-        print(row[4], "-", row[1], "-", row[6])
+        print(row[3], "-", row[1], "-", row[6])
 
 output = open("output.txt", "w")
 output_count = 0
@@ -43,7 +43,7 @@ for index, row in enumerate(table):
     if index == 0:
         continue
 
-    if random() < 0.5 and output_count < 20:
+    if random() < 0.2 and output_count < 20:
         output_count += 1
         print(f"#{index} {row[4]}. {row[1]} - {row[6]}", file = output)
 
