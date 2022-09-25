2cd6eeae048d00639ab2fa57b03ab7f62f1b58e9
MikhailALKir <mixa_kirillov_53@mail.ru>
2022-09-25 14:05:32 +0300

    Lab №1

---
+++ main.py
@@ -0,0 +1,54 @@
+import csv
+from random import random
+
+file = open('books.csv', 'r')
+total_count = 0
+short_title_count = 0
+table = list(csv.reader(file, delimiter = ';'))
+
+for index, row in enumerate(table):
+    if index == 0:
+        continue
+
+    total_count += 1
+    title = row[1]
+
+    if len(title) > 30:
+        short_title_count += 1
+    total_count += 1
+
+print("Кол-во книг: ", total_count)
+print("Кол-во книг <30 символов: ", short_title_count)
+
+search_author = input('Введите автора: ').lower()
+
+for index, row in enumerate(table):
+    if index == 0:
+        continue
+
+    date = row[6]
+    year = int(date[6:10])
+
+    if year < 2018:
+        continue
+
+    author = row[3]
+
+    if author.lower() == search_author:
+        print(row[4], "-", row[1], "-", row[6])
+
+output = open("output.txt", "w")
+output_count = 0
+for index, row in enumerate(table):
+    if index == 0:
+        continue
+
+    if random() < 0.5 and output_count < 20:
+        output_count += 1
+        # <автор>. <название> - <год>
+        print(f"#{index} {row[4]}. {row[1]} - {row[6]}", file = output)
+
+output.close()
+file.close()
+
+
